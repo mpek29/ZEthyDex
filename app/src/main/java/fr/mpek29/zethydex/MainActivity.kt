543d5d9e01b85526712dc4133764677a4d802b9e
MPek29 <florianpasco@protonmail.com>
2022-08-08 15:38:44 +0200

    ZEthyDex : V0.3 : Add of a font, colors, icons, a logo for the application and an image that can serve as an example

---
+++ app/src/main/java/fr/mpek29/zethydex/MainActivity.kt
@@ -0,0 +1,12 @@
+package fr.mpek29.zethydex
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main) //We decide to display on the screen activity_main.xml
+    }
+
+}
